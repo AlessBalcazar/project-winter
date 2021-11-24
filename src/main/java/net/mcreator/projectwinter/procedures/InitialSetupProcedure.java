d08bede44cf85d6c2b61a095dc99df40770d699b
AlessBalcazar <alessbalkazar@outlook.com>
2021-11-23 19:47:19 -0700

    Creating initial setup for scoreboards

---
+++ src/main/java/net/mcreator/projectwinter/procedures/InitialSetupProcedure.java
@@ -0,0 +1,75 @@
+package net.mcreator.projectwinter.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.vector.Vector2f;
+import net.minecraft.entity.Entity;
+import net.minecraft.command.ICommandSource;
+import net.minecraft.command.CommandSource;
+
+import net.mcreator.projectwinter.ProjectwinterModVariables;
+import net.mcreator.projectwinter.ProjectwinterMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class InitialSetupProcedure {
+	@Mod.EventBusSubscriber
+	private static class GlobalTrigger {
+		@SubscribeEvent
+		public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+			Entity entity = event.getPlayer();
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("x", entity.getPosX());
+			dependencies.put("y", entity.getPosY());
+			dependencies.put("z", entity.getPosZ());
+			dependencies.put("world", entity.world);
+			dependencies.put("entity", entity);
+			dependencies.put("event", event);
+			executeProcedure(dependencies);
+		}
+	}
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				ProjectwinterMod.LOGGER.warn("Failed to load dependency x for procedure InitialSetup!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				ProjectwinterMod.LOGGER.warn("Failed to load dependency y for procedure InitialSetup!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				ProjectwinterMod.LOGGER.warn("Failed to load dependency z for procedure InitialSetup!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				ProjectwinterMod.LOGGER.warn("Failed to load dependency world for procedure InitialSetup!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((ProjectwinterModVariables.MapVariables.get(world).initial_setup == 0)) {
+			if (world instanceof ServerWorld) {
+				((World) world).getServer().getCommandManager().handleCommand(
+						new CommandSource(ICommandSource.DUMMY, new Vector3d(x, y, z), Vector2f.ZERO, (ServerWorld) world, 4, "",
+								new StringTextComponent(""), ((World) world).getServer(), null).withFeedbackDisabled(),
+						"scoreboard objectives add death_status deathCount");
+			}
+			ProjectwinterModVariables.MapVariables.get(world).initial_setup = (double) 1;
+			ProjectwinterModVariables.MapVariables.get(world).syncData(world);
+		}
+	}
+}
